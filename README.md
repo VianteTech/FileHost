56602d193df64d6239cc58dbfdd2652bc5253748
VianNateDave(s) <117189867+VianteTech@users.noreply.github.com>
2023-08-16 16:46:56 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# FileHost" 
+# FileHost
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
-let obj = require('../data/host.json')
-const fs = require('fs');
-const path = require('path')
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
+/* -- Modules and JSON Objects -- */
+let obj = require('./data/host.json')
+const port = process.env.PORT || 8080,
+      fs = require('fs'),
+      url = require('url'),
+      http = require('http'),
+      path = require('path'),
+      express = require('express'),
+      bodyParser = require('body-parser'),
+      app = express();
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(express.json());
-function DeployChanges(){
-    for(let i=0;i<obj.data.length;i++){
-        if(obj.extensions[i]=="image"){
+/* -- File Hosting -- */
+function DeployChanges(){//Create a function named DeployChanges.
+    for(let i=0;i<obj.data.length;i++){// For loop | We get the length of obj.data (From host/host.json with key:"data". 
+        if(obj.extensions[i]=="image"){//Check if each of the obj.extensions satisfies the condition.
+            //Create a variable named content ("Adding backticks").
             let content = 
 `<!DOCTYPE html> 
 <html lang="vi"> 
@@ -55,9 +61,9 @@ video,img{
 </style>
 </body> 
 </html>`
-            Host(obj.file[i],content)
+            Host(obj.file[i],content)//Call the function Host with 2 arguments.
         }
-        else if(obj.extensions[i]=="video"){
+        else if(obj.extensions[i]=="video"){//Same for the above.
             let content = 
 `<!DOCTYPE html> 
 <html lang="vi"> 
@@ -152,51 +158,41 @@ iframe{
 </style>
 </body> 
 </html>`
-            Host(obj.file[i],content)
+            Host(obj.file[i],content)//Same here as well.
         }
-        else{}
+        else{
+            return;
+        }//if none of the conditions satisfy, do nothing or you can add return;
     }
 }
-function Host(filename,content){
-    fs.writeFile(`/frontend/host/${filename}.html`, content, err => {
-        if (err) {
-        console.error(err);
-        }
+function Host(filename,content){//Create a function named Host with 2 arguments.
+    fs.writeFile(`./host/${filename}.html`, content, err => {//Create a file if it doesn't exist, else overwrite the file.
+        if (err)
+            console.error(err);
+        //Check if there is an error.
     });
 }
-// function CheckForMultipleFiles(){
-//     for(let i=0;i<obj.data.length;i++){
-//         for(let j=i+1;j<obj.data.length;j++){
-//             if(obj.data[i]==obj.data[j]){
-//                 obj.data.splice(j,1)
-//                 obj.extensions.splice(j,1)
-//                 obj.file.splice(j,1)
-//                 obj.base64.splice(j,1)
-//                 fs.writeFile('./data/host.json', JSON.stringify(obj,null,4), function writeJSON(err) {
-//                     if (err) return console.log(err);
-//                 });
-//             }
-//         }
-//     }
-// }Under Updating.
-function RequestChanges(arg,arg1){
-    newarg=arg.split("|")
-    newarg1=arg1.split("|")
-    for(let i=0;i<newarg.length;i++){
-        newarg[i]=newarg[i].trim()
-        //arg1 is base64 encoded
-        //arg is filename with extensions
+function RequestChanges(arg,arg1){//Create a function named RequestChanges with 2 arguments.
+    newarg=arg.split("|")//Create a new variable named newarg, here newarg is an array.
+    newarg1=arg1.split("|")//Same as above but the variable's name is newarg1.
+    for(let i=0;i<newarg.length;i++){//For Loop.
+        newarg[i]=newarg[i].trim()//We remove all the backspaces from the right and left.
         if(newarg1[i].slice(5,newarg1[i].indexOf(";"))=="text/plain" && path.parse(newarg[i]).ext==".txt"){
+            //Check if each of the elements of newarg1 satisfies the above conditions.
+            //We used path.parse(each of the elements of arg1).ext to get the extensions of the file. 
             obj["base64"].push(newarg1[i])
             obj["data"].push(newarg[i])
             obj["extensions"].push("text/plain")
             obj["file"].push(path.parse(newarg[i]).name)
+            //We push datas to each of the keys of obj.
             fs.writeFile('./data/host.json', JSON.stringify(obj,null,4), function writeJSON(err) {
-                if (err) return console.log(err);
+                if (err) return console.log(err);//Check if there is an error
             });
-            DeployChanges(); 
+            //Here host.json file will get some changes.
+            DeployChanges(); //Call function DeployChanges()
         }
         else if(newarg1[i].slice(5,newarg1[i].indexOf(";"))=="application/pdf" && path.parse(newarg[i]).ext==".pdf"){
+            //Same for the above.
             obj["base64"].push(newarg1[i])
             obj["data"].push(newarg[i])
             obj["extensions"].push("application/pdf")
@@ -204,8 +200,10 @@ function RequestChanges(arg,arg1){
             fs.writeFile('./data/host.json', JSON.stringify(obj,null,4), function writeJSON(err) {
                 if (err) return console.log(err);
             });
-            DeployChanges();        }
+            DeployChanges(); 
+        }
         else{
+            //Here as well
             obj["base64"].push(newarg1[i])
             obj["data"].push(newarg[i])
             obj["extensions"].push(newarg1[i].slice(5,newarg1[i].indexOf("/")))
@@ -215,12 +213,14 @@ function RequestChanges(arg,arg1){
             });
             DeployChanges();
         }
-        // CheckForMultipleFiles();
     }
 }
-app.post('/file', (req, res) => {
-    RequestChanges(req.body.fname,req.body.dlink)
-    res.send(
+app.post('/file', (req, res) => {//When you submitted the form, the values will be transfered to server.js.
+    RequestChanges(req.body.fname,req.body.dlink)//Call the function RequestChanges.
+    return;// return nothing.
+});
+app.get('/file', (req,res)=>{
+    return res.send(
 `<h1>
 Thank you for using, please wait for your deployment in 4 seconds.
 </h1>
@@ -229,10 +229,41 @@ Thank you for using, please wait for your deployment in 4 seconds.
     setTimeout(()=>{
         document.getElementById("wait").style.display="block";
     },4000)
-</script>
-`);
-    });
-const port = 8080;
-app.listen(port, () => {
-  console.log(`Server running on port 8080`);
+</script>`);
 });
+//If possible, when you submitted the form, you'll be directed.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+})
+//Home page.
+app.get('/host', (req, res)=>{
+    res.writeHead(200, {
+        'Content-Type': 'application/json'
+    });
+    fs.readFile('./data/host.json',null, function (err, json){
+        if (err) {
+            res.writeHead(404);
+            res.write('404|File Not Found');
+        }else{
+            res.write(json);
+        }
+        res.end()
+    })
+})// Just an example for Home Page, but you can see the host.json data.
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});//Listen to Port 8080.
+app.get('*', (req, res) => {
+    var q = url.parse(req.url,true);
+    var filename = "./host" +q.pathname;
+    fs.readFile(filename,function(err,data){
+        if(err){
+            res.writeHead(404,{'Content-Type':'text/html'});
+            return res.end("Page Not Found!")
+        }
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+    })
+})
+//Check all requests.
